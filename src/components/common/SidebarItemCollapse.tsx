45adf4feded16047799d24c1bd5a21ef850f36ab
buithanhbavuong <bavuongco10@gmail.com>
2023-06-08 15:29:26 +0700

    fix: Remove poper

--- src/components/common/SidebarItemCollapse.tsx
+++ src/components/common/SidebarItemCollapse.tsx
@@ -21,108 +21,7 @@ type Props = {
 const SidebarItemCollapse = ({item, root, setActiveItem, activeItem, compact }: Props) => {
   const [open, setOpen] = useState(false);
   const appState = useAtomValue(routeAtom);
-  
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const handleClickCompact = (event: React.MouseEvent<HTMLElement>) => {
-    setActiveItem(item.state);
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-  };
-  const openPopper = Boolean(anchorEl);
-  
-  
-  
-  const [anchorEl2, setAnchorEl2] = React.useState<null | HTMLElement>(null);
-  const handleClickCompact2 = (event: React.MouseEvent<HTMLElement>) => {
-    setActiveItem(item.state);
-    setAnchorEl2(anchorEl2 ? null : event.currentTarget);
-  };
-  const openPopper2 = Boolean(anchorEl2);
 
-  const popper2 = (
-    <Popper open={openPopper2} anchorEl={anchorEl2} placement="right-start" sx={{
-      left: "20px !important",
-    }}>
-      <Box sx={{
-        padding: "24px",
-        width: "282px",
-        height: "330px",
-        background: "#364BB5",
-        boxShadow: "0px 2px 6px 2px rgba(167, 167, 167, 0.5)",
-        borderRadius: "1rem"
-      }}>
-        <div style={{
-          fontStyle: "normal",
-          fontWeight: "500",
-          fontSize: "18px",
-          lineHeight: "23px",
-          display: "flex",
-          alignItems: "center",
-          color: "#FFFFFF"
-        }}>
-          <List sx={{ width: "100%"}}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                </ListItemIcon>
-                <ListItemText primary="Inbox" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                </ListItemIcon>
-                <ListItemText primary="Drafts" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </div>
-      </Box>
-    </Popper>
-  )
-  
-  const popper = (
-    <Popper open={openPopper} anchorEl={anchorEl} placement="right-start" sx={{
-      left: "20px !important",
-    }}>
-      <Box sx={{
-        padding: "24px",
-        width: "282px",
-        height: "330px",
-        background: "#364BB5",
-        boxShadow: "0px 2px 6px 2px rgba(167, 167, 167, 0.5)",
-        borderRadius: "1rem"
-      }}>
-        <div style={{
-          fontStyle: "normal",
-          fontWeight: "500",
-          fontSize: "18px",
-          lineHeight: "23px",
-          display: "flex",
-          alignItems: "center",
-          color: "#FFFFFF"
-        }}>
-          <List sx={{ width: "100%"}}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                </ListItemIcon>
-                <ListItemText primary="Inbox" />
-                {/*{compact && popper2}*/}
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                </ListItemIcon>
-                <ListItemText primary="Drafts" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </div>
-      </Box>
-    </Popper>
-  )
-  
   const handleClick = () => {
     setOpen(!open);
     setActiveItem(item.state);
@@ -147,7 +46,7 @@ const SidebarItemCollapse = ({item, root, setActiveItem, activeItem, compact }: 
   return (
     <ListItemButtonContainer bottom={item.stickToBottom} root={root}>
       <StyledListItemButton
-        onClick={compact ? handleClickCompact : handleClick}
+        onClick={handleClick}
         selected={appState.includes(item.state) && !open}
         active={appState.includes(item.state) && open}
         variant={root ? "primary" : "secondary"}
@@ -162,7 +61,6 @@ const SidebarItemCollapse = ({item, root, setActiveItem, activeItem, compact }: 
         />}
         {!compact ? (open) ? <ExpandLessOutlinedIcon/> : <ExpandMoreOutlinedIcon/> : null}
       </StyledListItemButton>
-      {compact && popper}
       {!compact && <Collapse in={open} timeout="auto">
         <List>
           {item.child?.map((route, index) => {
